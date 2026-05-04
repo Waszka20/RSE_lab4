ef4a53901a2d341d87aec0f797160e6e235983ab
piotr <piotrwaszak@student.agh.edu.pl>
2026-05-04 08:18:40 +0200

    123

--- zad.py
+++ zad.py
@@ -1,11 +1,16 @@
-import pint
-import numpy as np
-from scipy import constants
+import random
 
-si = pint.UnitRegistry()
+def estimate_pi(num_samples=1000000):
+    inside_circle = 0
 
-def equilibrium_vapour_pressure(si, T):
-    """ Teten's formula (https://en.wikipedia.org/wiki/Tetens_equation) """
-    TC = T - constants.zero_Celsius * si.K
-    return .61078 * si.kPa * np.exp(17.27 * TC / (237.3 * si.K + TC))
+    for _ in range(num_samples):
+        x = random.random()  # random number in [0,1)
+        y = random.random()
+        
+        # Check if the point is inside the unit quarter circle
+        if x**2 + y**2 <= 1:
+            inside_circle += 1
+
+    # Ratio of points inside circle to total points
+    return 4 * inside_circle / num_samples
 
